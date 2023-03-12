dce260b02a9b591beb000a2a205c6d2427ffae60
roRickard <124548135+roRickard@users.noreply.github.com>
2023-03-12 11:47:22 -0300

    Move variables for player selections to playRound function

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
-let choices = ['rock', 'paper', 'scissors'];
-let playerSelect = null;
-let computerSelect = choices[Math.floor(Math.random() * 3)];
+// Define an array of choices for the game
+const choices = ['rock', 'paper', 'scissors'];
+
+// Initialize variables to store player and computer selections
+
+
+// Initialize variables to track the game progress and scores
 let gameTracker = 0;
 let playerWins = 0
 ,   computerWins = 0
 ,   ties = 0;
 
-console.log(computerSelect);
-console.log(playerSelect);
-
-function playRound() {
+// Function to play one round of the game
+//Initialize variables to store player and computer selections
+function playRound(playerSelect, computerSelect) {
+    // Check if it's a tie
     if (playerSelect === computerSelect) {
         alert('It was a tie!');
         ties ++; 
         } else if (
+            // Check if player wins
             (playerSelect === 'rock' && computerSelect === 'scissors') 
         || 
             (playerSelect === "scissors" && computerSelect === 'paper')
@@ -22,22 +27,27 @@ function playRound() {
                 alert('You win!');
                 playerWins++;
         } else {
+            // Otherwise, computer wins
             alert('You lose!')
             computerWins++;
     }
 }
 
+// Play 5 rounds of the game
 for (let gamesPlayed = 0; gamesPlayed < 5; gamesPlayed++) {
-    
-    console.log(playerWins)
-    console.log(computerWins)
     playerSelect = prompt('Please enter rock paper or scissors').toLowerCase();
+    // Generate random computer selection from choices array
     computerSelect = choices[Math.floor(Math.random() * 3)];
+    // Increment game tracker and play one round of the game
     gameTracker++;
     playRound();
+    // Log current scores to console
+    console.log(playerWins, computerWins, ties);
 }
 
+// After 5 rounds, display final score using an alert
 while (gameTracker === 5) {
     alert(`The final score is: ${playerWins} win(s) for the player, and ${computerWins} for the computer. There was/were ${ties} tie(s).`);
+    // Break out of while loop
     break;
 }
