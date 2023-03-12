326d26161798c8905e380f9b3ae9a8de70d7e76b
roRickard <124548135+roRickard@users.noreply.github.com>
2023-03-12 11:27:05 -0300

    Create loop to play 5 rounds and report the score upon completion

--- js/index.js
+++ js/index.js
@@ -1,25 +1,43 @@
-let choices = ["rock", "paper", "scissors"];
-
-
-let playerSelect = prompt("Please enter rock paper or scissors").toLowerCase();
+let choices = ['rock', 'paper', 'scissors'];
+let playerSelect = null;
 let computerSelect = choices[Math.floor(Math.random() * 3)];
+let gameTracker = 0;
+let playerWins = 0
+,   computerWins = 0
+,   ties = 0;
+
 console.log(computerSelect);
 console.log(playerSelect);
 
 function playRound() {
     if (playerSelect === computerSelect) {
-        alert("It was a tie!"); 
+        alert('It was a tie!');
+        ties ++; 
         } else if (
-            (playerSelect === "rock" && computerSelect === "scissors") 
+            (playerSelect === 'rock' && computerSelect === 'scissors') 
         || 
-            (playerSelect === "scissors" && computerSelect === "paper")
+            (playerSelect === "scissors" && computerSelect === 'paper')
         ||
-            (playerSelect === "paper" && computerSelect === "rock")) {
-                alert("You win!");
+            (playerSelect === 'paper' && computerSelect === 'rock')) {
+                alert('You win!');
+                playerWins++;
         } else {
-           console.log(computerSelect);
+            alert('You lose!')
+            computerWins++;
     }
 }
 
+for (let gamesPlayed = 0; gamesPlayed < 5; gamesPlayed++) {
+    
+    console.log(playerWins)
+    console.log(computerWins)
+    playerSelect = prompt('Please enter rock paper or scissors').toLowerCase();
+    computerSelect = choices[Math.floor(Math.random() * 3)];
+    gameTracker++;
+    playRound();
+}
 
-playRound();
+while (gameTracker === 5) {
+    alert(`The final score is: ${playerWins} win(s) for the player, and ${computerWins} for the computer. There was/were ${ties} tie(s).`);
+    break;
+}
