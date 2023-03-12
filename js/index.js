27bf71ac076a2729c8d25c99b5eb378c7522766b
roRickard <124548135+roRickard@users.noreply.github.com>
2023-03-12 10:44:35 -0300

    Create funtion to play round of rock paper scissors by comparing playerSelect and computerSelect

--- js/index.js
+++ js/index.js
@@ -6,3 +6,20 @@ let computerSelect = choices[Math.floor(Math.random() * 3)];
 console.log(computerSelect);
 console.log(playerSelect);
 
+function playRound() {
+    if (playerSelect === computerSelect) {
+        alert("It was a tie!"); 
+        } else if (
+            (playerSelect === "rock" && computerSelect === "scissors") 
+        || 
+            (playerSelect === "scissors" && computerSelect === "paper")
+        ||
+            (playerSelect === "paper" && computerSelect === "rock")) {
+                alert("You win!");
+        } else {
+           console.log(computerSelect);
+    }
+}
+
+
+playRound();
